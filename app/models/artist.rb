62513862e09dcf797a911e318ba06aa3398f3dc4
Lili-Mae <lilimae@hotmail.co.uk>
2020-01-14 17:57:11 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,43 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def galleries
+    self.paintings.map { |painting| painting.gallery }
+  end
+
+  def cities
+    self.galleries.map { |gallery| gallery.city}
+  end
+
+  def self.total_experience
+    sum_years_experience = @@all.map do |artist|
+      artist.years_experience
+    end
+    sum_years_experience.sum
+  end
+  
+  def self.most_prolific
+    self.all.max_by { |artists| artists.years_experience / artists.paintings.count }
+  end
+
+  def self.create_painting(title, price, gallery)
+    Painting.new("title", "price", self, "gallery")
   end
 
 end

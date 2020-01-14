3692ecd67294d7aea7d327c36016d28f01ab43cd
Sergio Robles <sroblesc@gmail.com>
2020-01-14 12:52:16 +0000

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,39 @@
 class Gallery
 
-  attr_reader :name, :city
+  attr_reader :name, :city, :painting
+
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+  #allways create the following code so I can use it in other methods. It connect the paintings.all con galleries.
+  def paintings 
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  def artists 
+    self.paintings.map {|painting| painting.artist}
+  end
+
+  def artists_names(name)
+    self.artists.map do |artist|
+      artist.name
+    end
+  end
+
+  def most_expensive_painting(price)
+    paintings.max_by do |painting|
+      painting.price
+    end
   end
 
 end
